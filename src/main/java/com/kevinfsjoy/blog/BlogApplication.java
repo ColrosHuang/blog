541553f98cc03e0b44ed06d8be3419f79b689908
kevin <kevin@kevins-MacBook-Pro.local>
2018-11-09 00:04:04 +0800

    Initial commit

---
+++ src/main/java/com/kevinfsjoy/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package com.kevinfsjoy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.kevinfsjoy.controller")
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
